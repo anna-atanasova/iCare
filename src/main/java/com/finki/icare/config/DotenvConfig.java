5dbfdcf0bd143964412a92dad9783fb6f0509f19
186079 <matej.milevski@students.finki.ukim.mk>
2025-12-16 17:57:15 +0100

    Add dotenv deps

---
+++ src/main/java/com/finki/icare/config/DotenvConfig.java
@@ -0,0 +1,31 @@
+package com.finki.icare.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DotenvConfig implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        Map<String, Object> dotenvProperties = new HashMap<>();
+
+        dotenv.entries().forEach(entry -> {
+            dotenvProperties.put(entry.getKey(), entry.getValue());
+        });
+
+        environment.getPropertySources()
+                .addFirst(new MapPropertySource("dotenvProperties", dotenvProperties));
+    }
+}
